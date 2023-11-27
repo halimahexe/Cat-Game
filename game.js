f1686c5ec26aec9f0be1fde7b763d23089571172
halimahexe <127022084+halimahexe@users.noreply.github.com>
2023-11-27 18:32:53 +0000

    deleting some unnecessary code

--- game.js
+++ game.js
@@ -9,15 +9,18 @@ let cat; // This will be the cat randomly selected for the game
 const image = document.querySelector('#image');
 const catPic = '<svg xmlns="http://www.w3.org/2000/svg" height="5rem" viewBox="0 0 576 512" alt="icon of cat"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M320 192h17.1c22.1 38.3 63.5 64 110.9 64c11 0 21.8-1.4 32-4v4 32V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V339.2L280 448h56c17.7 0 32 14.3 32 32s-14.3 32-32 32H192c-53 0-96-43-96-96V192.5c0-16.1-12-29.8-28-31.8l-7.9-1c-17.5-2.2-30-18.2-27.8-35.7s18.2-30 35.7-27.8l7.9 1c48 6 84.1 46.8 84.1 95.3v85.3c34.4-51.7 93.2-85.8 160-85.8zm160 26.5v0c-10 3.5-20.8 5.5-32 5.5c-28.4 0-54-12.4-71.6-32h0c-3.7-4.1-7-8.5-9.9-13.2C357.3 164 352 146.6 352 128v0V32 12 10.7C352 4.8 356.7 .1 362.6 0h.2c3.3 0 6.4 1.6 8.4 4.2l0 .1L384 21.3l27.2 36.3L416 64h64l4.8-6.4L512 21.3 524.8 4.3l0-.1c2-2.6 5.1-4.2 8.4-4.2h.2C539.3 .1 544 4.8 544 10.7V12 32v96c0 17.3-4.6 33.6-12.6 47.6c-11.3 19.8-29.6 35.2-51.4 42.9zM432 128a16 16 0 1 0 -32 0 16 16 0 1 0 32 0zm48 16a16 16 0 1 0 0-32 16 16 0 1 0 0 32z"/></svg>'
 
+// Buttons
+
 const interact1 = document.querySelector("#interact-btn1");
 const interact2 = document.querySelector("#interact-btn2");
 const interact3 = document.querySelector("#interact-btn3");
 const interact4 = document.querySelector("#interact-btn4");
-
 const start = document.querySelector("#startBtn");
 const end = document.querySelector('#endDay');
 const next = document.querySelector('#nextDay');
 const restart = document.querySelector('#restart');
+
+// Text containers
 
 const energyText = document.querySelector("#energy");
 const happyText = document.querySelector("#happy");
@@ -41,7 +44,7 @@ const cats = [{
 "Enzo purrs as you pet him, rubbing his cheeks against your hand.",
 "Enzo burbles as you wave a feather toy at him, eyeing it up before he pounces and catches it with his paws.",
 "Enzo eyes you warily as you bring out the claw trimmers before tucking his paws under his body and out of reach."],
-    points: [10, 10, 15, 5], // Get less points for 'care' because that entails discomfort
+    points: [25, 10, 15, 5], // Get less points for 'care' because that entails discomfort
     multiplier: [1, 1.5, 1, 0.25],
     colour: "#000000"
 }, {
@@ -50,7 +53,7 @@ const cats = [{
     typeText: "An aloof cat pretends that humans are beneath them but they enjoy your care nonetheless. You will get less points for some actions and more for others. It's your job to figure out which!",
     interactions: ["feed", "pet", "play", "care"],
     interactText: ["Cleo is pleased that you deigned to finally feed her.","Cleo has decided to allow you to touch her fluffyness ... for now.","Cleo is excited to use her sharp claws to destroy her prey.","Cleo is displeased by your attempts to coerce her into submission, even if it is for 'the greater good', whatever that is..."],
-    points: [30, 10, 15, 10],
+    points: [25, 10, 15, 10],
     multiplier: [1, 0.5, 1, 0.5],
     colour: "#E3963E"
 }, {
@@ -59,7 +62,7 @@ const cats = [{
     typeText: "A friendly cat is uncomplicated in their desire for love and affection and will give you the same points for all actions.",
     interactions: ["feed", "pet", "play", "care"],
     interactText: ["Mungo is happy you fed him.", "Mungo purrs as you stroke his soft fur. He's blissful.", "Mungo waits for the feather toy to be within paw's reach and bats it away.", "Mungo is happy to be taken care of.",],
-    points: [15, 15, 15, 15], // Friendly cats give the same amount of points for everything
+    points: [25, 15, 15, 15], // Friendly cats give the same amount of points for everything
     multiplier: [1, 1, 1, 1],
     colour: "#808080"
 }];
@@ -111,7 +114,6 @@ function feed() {
             actions--;
             fullText.innerText = full;
             actionsText.innerText = actions;
-            energyText.innerText = energy;
             textOne.innerText = `You fed ${cat.name}.`
             textTwo.innerText = cat.interactText[0];
         } else {
@@ -132,7 +134,6 @@ function pet() {
             actions--;
             happyText.innerText = happiness;
             actionsText.innerText = actions;
-            energyText.innerText = energy;
             textOne.innerText = `You pet ${cat.name}.`
             textTwo.innerText = cat.interactText[1];
         } else {
