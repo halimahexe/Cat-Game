8420d1bb5ca8ef94ebd1fa3e9775bd6fdd2d37de
halimahexe <127022084+halimahexe@users.noreply.github.com>
2023-11-29 13:31:03 +0000

    more attempts to refactor that didn't work

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+// Declaring variables
 let energy;
 let happiness;
 let full;
@@ -10,7 +11,7 @@ const image = document.querySelector('#image');
 const catPic = '<svg xmlns="http://www.w3.org/2000/svg" height="5rem" viewBox="0 0 576 512" alt="icon of cat"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M320 192h17.1c22.1 38.3 63.5 64 110.9 64c11 0 21.8-1.4 32-4v4 32V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V339.2L280 448h56c17.7 0 32 14.3 32 32s-14.3 32-32 32H192c-53 0-96-43-96-96V192.5c0-16.1-12-29.8-28-31.8l-7.9-1c-17.5-2.2-30-18.2-27.8-35.7s18.2-30 35.7-27.8l7.9 1c48 6 84.1 46.8 84.1 95.3v85.3c34.4-51.7 93.2-85.8 160-85.8zm160 26.5v0c-10 3.5-20.8 5.5-32 5.5c-28.4 0-54-12.4-71.6-32h0c-3.7-4.1-7-8.5-9.9-13.2C357.3 164 352 146.6 352 128v0V32 12 10.7C352 4.8 356.7 .1 362.6 0h.2c3.3 0 6.4 1.6 8.4 4.2l0 .1L384 21.3l27.2 36.3L416 64h64l4.8-6.4L512 21.3 524.8 4.3l0-.1c2-2.6 5.1-4.2 8.4-4.2h.2C539.3 .1 544 4.8 544 10.7V12 32v96c0 17.3-4.6 33.6-12.6 47.6c-11.3 19.8-29.6 35.2-51.4 42.9zM432 128a16 16 0 1 0 -32 0 16 16 0 1 0 32 0zm48 16a16 16 0 1 0 0-32 16 16 0 1 0 0 32z"/></svg>'
 
 // Buttons
-
+// const interactBtns = document.querySelectorAll('[id^="interact-btn"]'); // This was used for my attempt to make my code less repetitive
 const interact1 = document.querySelector("#interact-btn1");
 const interact2 = document.querySelector("#interact-btn2");
 const interact3 = document.querySelector("#interact-btn3");
@@ -21,7 +22,6 @@ const next = document.querySelector('#nextDay');
 const restart = document.querySelector('#restart');
 
 // Text containers
-
 const energyText = document.querySelector("#energy");
 const happyText = document.querySelector("#happy");
 const fullText = document.querySelector("#full");
@@ -66,6 +66,9 @@ const cats = [{
         "care": "Mungo is happy to be taken care of."},
     colour: "#808080"
 }];
+
+// const interactFunc = {interact1: feed, interact2: pet, interact3: play, interact4: care};
+// I tried to create an object of functions that I could pass through an event listener to run functions depending on id of button clicked, but it didn't run the function
 
 // Click listeners lead to functions
 interact1.onclick = feed;
@@ -280,3 +283,16 @@ function noActions() {
 // function feed() {
 //     update(stats[0], 0);
 // }
+
+// Another attempt to avoid repeating the if (actions > 0) code in every function
+
+// interactBtns.forEach(function(interactBtn) {
+//     interactBtn.addEventListener('click', function(event) {
+//         let num = event.target.id.match(/\d/);
+
+//         if (actions > 0) {
+//             interactFunc[`interact${num}`];
+//             console.log(interactFunc[`interact${num}`]); // Console logging shows me the function but for some reason the function isn't running?
+//         } else noActions();
+//     })
+// });
